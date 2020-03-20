380e3722de363f728f6c5a4b3b57d32aa56a4835
leonardofaria00 <leonardofaria00@gmail.com>
2020-03-20 15:31:40 -0300

    [UPDATE] Incluindo Module HttpClient para acesso Rest API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { CicloVidaComponent } from './ciclo-vida/ciclo-vida.component';
 import { CursoModule } from './curso/curso.module';
 import { EventBindingComponent } from './event-binding/event-binding.component';
-import { MeuPrimeiroComponent } from './meu-componente/meu-primeiro.component';
-import { PropertyBindComponent } from './property-bind/property-bind.component';
 import { InputOutputPropertyComponent } from './input-output-property/input-output-property.component';
-import { CicloVidaComponent } from './ciclo-vida/ciclo-vida.component';
+import { MeuPrimeiroComponent } from './meu-componente/meu-primeiro.component';
+import { PessoaComponent } from './pessoa/pessoa.component';
+import { PropertyBindComponent } from './property-bind/property-bind.component';
 
 
 
@@ -19,10 +21,12 @@ import { CicloVidaComponent } from './ciclo-vida/ciclo-vida.component';
     EventBindingComponent,
     PropertyBindComponent,
     InputOutputPropertyComponent,
-    CicloVidaComponent
+    CicloVidaComponent,
+    PessoaComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     CursoModule,
     FormsModule
   ],
